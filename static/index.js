579b291b4df0fb2d40952182a2f6674f362e1f3b
Masaks01 <117779863+Masaks01@users.noreply.github.com>
2025-03-28 10:46:22 +0100

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,6 @@
 function fortsett() {
     const username = document.getElementById("username").value.trim();
     if (username) {
-      // Valgfritt: lagre i localStorage hvis du vil bruke det videre
       localStorage.setItem("brukernavn", username);
       window.location.href = "/dashbord";
     } else {
