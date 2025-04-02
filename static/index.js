d186829b35d4b3ad410dc8354d41cb92be237a1b
Masaks01 <117779863+Masaks01@users.noreply.github.com>
2025-04-02 14:38:02 +0200

    Delete static/index.js

--- static/index.js
+++
@@ -1,9 +0,0 @@
-function go() {
-    const username = document.getElementById("username").value.trim();
-    if (username) {
-      localStorage.setItem("brukernavn", username);
-      window.location.href = "/dashbord";
-    } else {
-      alert("Vennligst skriv inn brukernavn.");
-    }
-  }
